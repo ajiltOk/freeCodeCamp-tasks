8c80d3fb069e9c57a2c08c35f218f3436672f828
ajiltOk <112711151+ajiltOk@users.noreply.github.com>
2023-06-19 15:03:32 +0300

    refactor: update and rename task

--- everything-be-true.js
+++ everything-be-true.js
@@ -1,20 +1,13 @@
 //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/everything-be-true
 
 function truthCheck(collection, pre) {
-  let result = [];
+  let arr = [];
   collection.forEach(element => {
-    if(element[pre]) {
-      result.push(true);
-    } else {
-      result.push(false);
-    }
+    element[pre] ? arr.push(true) : arr.push(false);
   })
   
-  if(result.includes(false)) {
-    return false;
-  } else {
-    return true;
-  }
+  let result = arr.includes(false) ? false : true;
+  return result;
 }
 
 truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
