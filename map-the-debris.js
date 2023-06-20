d5c5dbb5a7d173c3d42e702aa8bd606d07af4bf6
ajiltOk <112711151+ajiltOk@users.noreply.github.com>
2023-06-20 16:19:13 +0300

    Update map-the-debris.js

--- map-the-debris.js
+++ map-the-debris.js
@@ -1,19 +1,1 @@
-// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/map-the-debris
 
-function orbitalPeriod(arr) {
-  let avgAltArr = [];
-  const GM = 398600.4418;
-  const earthRadius = 6367.4447;
-  
-  arr.forEach(element => {
-    avgAltArr.push(element.avgAlt);
-    delete element.avgAlt;
-  })
-  
-  for(let i = 0; i < avgAltArr.length; i++) {
-    arr[i].orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow((earthRadius + avgAltArr[i]), 3) / GM));
-  }
-  return arr;
-}
-
-orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
